78ab27a7b0b95292f02781e78daea63371aaa82d
Landry <landos@octangrp.com>
2020-03-24 08:39:06 +0200

    change services to what we do

--- src/components/sections/service.js
+++ src/components/sections/service.js
@@ -6,9 +6,9 @@ import React from "react"
 const Service = ({ siteTitle }) => (
 
     <div class="panel z-10 px-0 pt-32 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
-  <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-90 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 mx-auto md:w-90 xs:w-100 sm:w-95 xl:flex md:flex  lg:flex sm:flex relative">
+  <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-90 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 mx-auto md:w-90 xs:w-100 sm:w-95 relative">
       
-
+  <h1 class="text-center font-semiBold text-4xl pb-24">What We Do</h1>
       <div class="flex align-items-end relative w-100 md:w-55 pb-4 xs:w-95 mx-auto">
         <div class="w-60 text-center">
         <img src={'/img/Untitled-1.png'}/>
