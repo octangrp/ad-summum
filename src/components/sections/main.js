c4cea5d741340e63f6ff002b3e97c9d83bb47e2d
Landry <landos@octangrp.com>
2020-03-25 08:22:13 +0200

    create the language switcher

--- src/components/sections/main.js
+++ src/components/sections/main.js
@@ -6,7 +6,7 @@ import React from "react"
 const Main = ({ siteTitle }) => (
 
   <div class="panel z-10 px-0 pt-32 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
-  <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-95 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 ml-auto md:w-90 xs:w-100 sm:w-95 xl:flex md:flex  lg:flex sm:flex relative">
+  <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-95 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 ml-auto md:w-90 xs:w-100 sm:w-95  relative">
       <div class="flex align-items-end relative w-100 md:w-55 pb-4 xs:w-95 mx-auto">
           <div class="w-50 pt-16">
              <div class="pt-5">
@@ -23,6 +23,10 @@ your leads come from, them.</p>
           <div class="w-50 text-right pt-16">
             <img src={'/img/group.png'}/>
           </div> 
+          </div>
+
+          <div class="text-right w-70 ml-auto mr-12">
+          <img src={'/img/rw-2009.gif'} class="w-5" />
           </div>
        
   </div>
