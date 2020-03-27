167b672adcef0498f46d056ba267aedd4fb66493
Yannick <musayann@octangrp.com>
2020-03-27 11:05:36 +0200

    Add sass loader to compile og-css varibles
    Added a sass loader plugin allowing now to load og-css sass varibles
    allowing to customize the default color variables and fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,12 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    }{
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
     },
-    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
