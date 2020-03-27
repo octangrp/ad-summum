e2e4e0f4dab61d2b782979ef1739cce7d38b16ae
Yannick <musayann@octangrp.com>
2020-03-27 20:36:26 +0200

    Add fonts and color varibles
    Added the webfonts  and update the platform colors to match the mockup

--- src/components/sections/information.js
+++ src/components/sections/information.js
@@ -14,53 +14,45 @@ const Information = ({ siteTitle }) => (
 
   <div class="w-85 mx-auto">
 
-    <h1 class="text-center text-4xl pb-24">Discover</h1>
+    <h4 class="text-center text-2xl pb-24 font-bold text-primary font-secondary">Discover</h4>
+        <div class="relative w-100">
+        <div class="w-rem-88 h-rem-88 border-8 border-grey relative">
 
-      <div class="w-50 h-60 border-8 border-grey relative">
-
-          <div class="pt-4 px-5 relative">
-            <ul class="list text-center w-100 lg:w-60  mr-auto p-0 m-0 hidden xl:block lg:block px-0 mr-0  text-sm">
-              <li class="inline-block my-0 mr-8 text-5xl text-grey font-bold">1.</li>
-              <li class="inline-block my-0 mr-8 text-5xl text-orange font-boldunderline">2.</li>
-              <li class="inline-block my-0 mr-8 text-5xl text-grey font-bold">3.</li>
-              <li class="inline-block my-0 mr-8 text-5xl text-grey font-bold">5.</li>
-              </ul>
-          </div>
-
-      </div>
-
-        <div class="w-60 h-30 bg-white absolute t-40 r-20">
-          <p class="text-black font-prina font-bold text-6xl">Discover where your leads come from, 
-what it costs to getthem,  and how they 
-interact with your websitebefore contacting you. 
-</p>
+        <div class="pt-4 px-5 relative">
+          <ul class="list text-center w-100 lg:w-60  font-primary mr-auto p-0 m-0 hidden xl:block lg:block px-0 mr-0  text-sm">
+            <li class="inline-block my-0 py-1 mr-8 text-3xl text-grey font-bold">1.</li>
+            <li class="inline-block my-0 py-1 mr-8 text-3xl text-primary font-bold border-0 border-b-4 border-solid border-grey">2.</li>
+            <li class="inline-block my-0 py-1 mr-8 text-3xl text-grey font-bold">3.</li>
+            <li class="inline-block my-0 py-1 mr-8 text-3xl text-grey font-bold">4.</li>
+            </ul>
         </div>
+
+        </div>
+        <div class="w-60  bg-white absolute t-25 mx-16 py-10">
+        <p class="text-black font-secondary font-bold text-2xl my-0">Discover where your leads come from, 
+        what it costs to getthem,  and how they 
+        interact with your websitebefore contacting you. 
+        </p>
+        </div>
+        </div>
+
 
         <div class="flex w-100 text-center relative pt-12">
-        <div class="w-50 px-4">
-          <div class="mt-0 mb-0 h-rem-12 w-rem-14 border-2 border-solid border-red ml-auto text-center flex align-items-center justify-content-center text-2xl text-back">
-            <span class="animated fadeOutLeft custom-classes-transition-leave-to">
-            <FontAwesomeIcon icon="coffee" />
-            </span></div>
+        <div class="w-50 px-4 text-right">
+          <button class="btn  bg-transparent hover:bg-primary hover:text-white cursor-pointer text-primary py-1 px-4 border-primary border-6 text-2xl">
+              <FontAwesomeIcon icon="arrow-left" />
+          </button>
         </div>
-        <div class="w-50 px-4">
-        <div class="mt-0 mb-0 h-rem-12 w-rem-14 border-2 border-solid border-red mr-auto text-center flex align-items-center justify-content-center text-2xl text-black">
-            <span class="animated fadeOutLeft custom-classes-transition-leave-to">
-            
-            <FontAwesomeIcon icon="right" />
-            </span></div>
+        <div class="w-50 px-4 text-left">
+           <button class="btn  bg-transparent hover:bg-primary hover:text-white cursor-pointer text-primary py-1 px-4 border-primary border-6 text-2xl">
+              <FontAwesomeIcon icon="arrow-right" />
+          </button>
         </div>
-      </div>
-
-      </div>
-
-     
-
-      
-       
+      </div>  
   </div>
 
 
+  </div>
 </div>
 
     
