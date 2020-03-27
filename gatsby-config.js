8c0293010561bb669802f204cc3ae334eacc3458
Yannick <musayann@octangrp.com>
2020-03-27 11:10:00 +0200

    Load all fontawesome icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    }{
+    },
+    {
       resolve: `gatsby-plugin-sass`,
       options: {
         implementation: require("sass"),
