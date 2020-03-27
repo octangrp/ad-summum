f0377886edb3058c34faf2f1f5d4e94a31c5aecf
Yannick <musayann@octangrp.com>
2020-03-27 22:04:51 +0200

    Allow rendering multiple services
    Added the option to render multiple services and activated the
    pagination dots

---
+++ src/components/cards/service.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+
+const ServiceCard = ({ title, image, children }) => (
+
+<div class="flex align-items-end relative w-100 md:w-55 pb-4 xs:w-95 mx-auto">
+        <div class="w-60 text-center">
+        <img src={image}/>
+        </div>
+        <div class="w-40">
+            <div class="pt-16">
+                <h4 class="text-primary text-2xl font-bold text-center font-secondary text-center font-light pt-4 leading-loose">{title}</h4>
+                 {children}
+                <div class="w-100 text-center pt-12">
+             <button class="btn bg-none  py-3 px-8 text-primary rounded-full border-1 border-solid border-primary text-xs text-light mx-auto">READ MORE</button>
+          </div>
+            </div>
+        </div>
+
+    </div>
+)
+
+ServiceCard.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+}
+  
+ServiceCard.defaultProps = {
+  title: `Information Consulting`,
+}
+  
+export default ServiceCard
+  
--- src/components/sections/service.js
+++ src/components/sections/service.js
@@ -1,53 +1,66 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import ServiceCard from "../cards/service"
+import ReactHtmlParser from 'react-html-parser'; 
 
+class Service  extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    }
+    this.switchSection = this.switchSection.bind(this);
+  }
 
-const Service = ({ siteTitle }) => (
+  switchSection(index) {
+    // Update state so the next render will show the fallback UI.
+    this.setState({index: index})
+  }
 
-    <div class="panel z-10 px-0 pt-32 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
-  <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-90 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 mx-auto md:w-90 xs:w-100 sm:w-95 relative">
-      
-  <h1 class="text-center font-semiBold text-4xl pb-24">What We Do</h1>
-      <div class="flex align-items-end relative w-100 md:w-55 pb-4 xs:w-95 mx-auto">
-        <div class="w-60 text-center">
-        <img src={'/img/Untitled-1.png'}/>
-        </div>
-        <div class="w-40">
-            <div class="pt-16">
-                <h4 class="text-primary text-2xl font-bold text-center font-secondary">Information Consulting</h4>
-                <p class="text-center font-light pt-4 leading-loose">
-                Discover where your leads come from, what it costs to 
-getthem,  and how they interact with your website before contacting you. 
-Discover where  your leads come from, them.
-                </p>
-                <div class="w-100 text-center pt-12">
-            <button class="btn bg-none  py-3 px-8 text-primary rounded-full border-1 border-solid border-primary text-xs text-light mx-auto">READ MORE</button>
+  currentService(){
+    if(this.state.index < this.props.services.length){
+        return this.props.services[this.state.index];
+    }
+    return null
+  }
+
+  render(){
+    return (
+      <div class="panel z-10 px-0 pt-32 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
+        <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-90 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 mx-auto md:w-90 xs:w-100 sm:w-95 relative">
+          <h1 class="text-center font-semiBold text-4xl pb-24">What We Do</h1>
+          <div>
+          { this.currentService() ? <ServiceCard key={this.state.index} title={this.currentService().title} image={this.currentService().image}>{ReactHtmlParser(this.currentService().body)}</ServiceCard> : null}
+          <div class="w-100 text-center mt-12">
+            {this.props.services.map((item, index) =>  <span key={index} onClick={()=> this.switchSection(index)} className={['inline-block h-rem-3 w-rem-3 border-1 border-solid border-primary rounded-full mx-4 cursor-pointer', this.state.index == index ? 'bg-primary' : 'bg-transparent'].join(' ') }></span>)}
           </div>
-            </div>
         </div>
-
+      </div>
     </div>
-
-    <div >
-
-    </div>
-
-       
-  </div>
-
-
-</div>
-
-    
-)
+   )
+  }  
+}
 
 Service.propTypes = {
   siteTitle: PropTypes.string,
+  services: PropTypes.array,
 }
 
 Service.defaultProps = {
   siteTitle: ``,
+  services: [
+  {
+    image: '/img/Untitled-1.png',
+    title: 'Information Consulting fsd',
+    body: "<p>Discover where your leads come from, what it costs to get them, and how they interact with your website before contacting you. Discover where  your leads come from, them.</p>"
+  },
+  {
+    image: '/img/Untitled-1.png',
+    title: 'Information Consultin dfsd',
+    body: "<p>Discover where your leads come from, what it costs to get them, and how they interact with your website before contacting you. Discover where  your leads come from, them.</p>"
+  }
+]
 }
 
 export default Service
