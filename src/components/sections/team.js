a57ac7dcafc0331e161e6853c8bd814ff87d1c61
Yannick <musayann@octangrp.com>
2020-03-28 12:50:18 +0200

    Fix linting issues

--- src/components/sections/team.js
+++ src/components/sections/team.js
@@ -9,7 +9,7 @@ const Team = ({ siteTitle }) => (
     <div class="panel mt-0 py-4 w-85 mx-auto md:w-95 xs:w-100 sm:w-100 centered">
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
@@ -18,7 +18,7 @@ const Team = ({ siteTitle }) => (
       </div>
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
@@ -27,7 +27,7 @@ const Team = ({ siteTitle }) => (
       </div>
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
@@ -36,7 +36,7 @@ const Team = ({ siteTitle }) => (
       </div>
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
@@ -45,7 +45,7 @@ const Team = ({ siteTitle }) => (
       </div>
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
@@ -54,7 +54,7 @@ const Team = ({ siteTitle }) => (
       </div>
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="h-rem-74 w-rem-74 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/lewis.jpg"}  alt="team-member"/>
+          <img src={"/img/lewis.jpg"} alt="team-member" />
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class="my-2">Gakindi Vincent</h5>
