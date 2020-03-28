327cd3b67dfa5f8a5e707ca6c5250b2bfc2755ea
karemarsy <karema@octangrp.com>
2020-03-28 16:37:13 +0200

    Fix file formatting

--- src/components/sections/main.js
+++ src/components/sections/main.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const Main = (props) => (
+const Main = props => (
   <div class="panel z-10 px-0 pt-32 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
     <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-95 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3  xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 ml-auto md:w-90 xs:w-100 sm:w-95 xl:flex md:flex  lg:flex sm:flex relative">
       <div class="flex align-items-end relative w-100 md:w-55 pb-4 xs:w-95 mx-auto">
@@ -10,10 +10,7 @@ const Main = (props) => (
             <h1 class="text-6xl pt-6 font-primary text-primary w-75">
               {props.title}
             </h1>
-            <div class="w-60 text-light pt-6 font-light">
-            {props.children}
-            </div>
-            
+            <div class="w-60 text-light pt-6 font-light">{props.children}</div>
           </div>
           <div class="w-100 pt-16 text-right">
             <button class="btn bg-primary py-3 px-12 text-white rounded-full text-xs text-light mx-auto shadow-primary">
--- src/components/sections/value.js
+++ src/components/sections/value.js
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const Value = ( props ) => (
-  <div id={props.id} class="panel z-10 px-0 pt-32 t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
+const Value = props => (
+  <div
+    id={props.id}
+    class="panel z-10 px-0 pt-32 t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto"
+  >
     <div class="panel mt-0 py-4 w-85 mx-auto md:w-95 xs:w-100 sm:w-100 centered">
       <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
         <div class="sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
@@ -10,10 +13,10 @@ const Value = ( props ) => (
         </div>
         <div class="panel-body bg-white center text-left py-2 px-0">
           <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
-          {props.title}
+            {props.title}
           </h5>
           <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
-          {props.children}
+            {props.children}
           </div>
         </div>
       </div>
@@ -23,10 +26,10 @@ const Value = ( props ) => (
         </div>
         <div class="panel-body bg-white center text-center py-2">
           <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
-          {props.title}
+            {props.title}
           </h5>
           <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
-          {props.children}
+            {props.children}
           </div>
         </div>
       </div>
@@ -36,53 +39,52 @@ const Value = ( props ) => (
         </div>
         <div class="panel-body bg-white center text-right py-2 px-0">
           <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
-          {props.title}
+            {props.title}
           </h5>
           <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
-          {props.children}
-          </div>
+            {props.children}
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
-        <div class="w-rem-30 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
-          <img src={"/img/Group-6.png"} />
-        </div>
-        <div class="panel-body bg-white center text-center py-2">
-          <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
+    <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
+      <div class="w-rem-30 sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden centered mx-auto my-2">
+        <img src={"/img/Group-6.png"} />
+      </div>
+      <div class="panel-body bg-white center text-center py-2">
+        <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
           {props.title}
-          </h5>
-          <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
+        </h5>
+        <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
           {props.children}
-          </div>
-          </div>
-        </div>
-      <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
-        <div class="sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden text-right mx-auto my-2">
-          <img src={"/img/Group-7.png"} />
-        </div>
-        <div class="panel-body bg-white center text-right py-2 px-0">
-          <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
-          {props.title}
-          </h5>
-          <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
-          {props.children}
-          </div>
         </div>
       </div>
+    </div>
+    <div class="panel panel-default p-2 xl:w-30 lg:w-45 md:w-45 sm:w-45 md:mr-2  rounded-lg overflow-hidden bg-transparent xl:inline-block lg:inline-block md:inline-block sm:inline-block sm:m-2 xl:m-2 lg:m-2 mt-3">
+      <div class="sm:h-64 sm:w-rem-64 xxs:h-48 xxs:w-rem-48 overflow-hidden text-right mx-auto my-2">
+        <img src={"/img/Group-7.png"} />
       </div>
-  
+      <div class="panel-body bg-white center text-right py-2 px-0">
+        <h5 class=" my-1 color-accent xl:pt-4 lg:pt-3 font-bold font-primary text-xl md:text-base xs:text-xs sm:text-base sm:ml-3 xs:text-center sm:my-3 xs:my-3">
+          {props.title}
+        </h5>
+        <div class="w-100  xl:pt-2 lg:pt-1 sm:ml-3 xs:text-center sm:text-xs md:text-sm text-lg left xs:text-xs xs:mx-auto xs:w-80 opacity-95">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 Value.propTypes = {
   siteTitle: PropTypes.string,
-  id : PropTypes.string,
+  id: PropTypes.string,
 }
 
 Value.defaultProps = {
   siteTitle: ``,
-  id : null
+  id: null,
 }
 
 export default Value
