39b484d86b8289160f93459bbd6af74a504c933f
Landry <landos@octangrp.com>
2020-04-16 17:50:17 +0200

    Remove the read more button and words spacing

--- src/components/sections/main.js
+++ src/components/sections/main.js
@@ -5,23 +5,30 @@ import ReactHtmlParser from "react-html-parser"
 const Main = props => (
   <div class="panel z-10 px-0 pt-32 xs:pt-0 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto">
     <div class="panel-body min-h-screen  sm:min-h-auto xs:min-h-auto h-100 w-95 lg:w-90 -mt-24 lg:-mt-32 md:-mt-3 xs:pt-0 xs:mt-5 lg:mt-3 lg:pt-3 p-0 sm:mt-5 ml-auto md:w-90 xs:w-100 sm:w-95 xl:flex md:flex  lg:flex sm:flex relative">
-      <div class="flex xs:block align-items-end relative w-100 md:w-55 pb-4 xs:w-90 mx-auto">
-        <div class="w-50 xs:w-100 pt-16 xs:pt-0">
-          <div class="pt-5 xs:pt-0">
-            <h1 class="text-6xl pt-6 font-primary text-primary w-75 xs:w-100 xs:text-3xl">
-              {props.title}
-            </h1>
-            <div class="w-60 text-light pt-6 xs:pt-0 xs:w-100 font-light">
+      <div class="flex xs:block align-items-end relative pt-16 w-100 md:w-55 pb-4 xs:w-90 mx-auto">
+        <div class="w-50 xs:w-100 xs:pt-0">
+          <div class=" xs:pt-0">
+            <div class="w-90 h-px-500 leafing-loose text-lg text-light xs:pt-0 xs:w-100 font-light">
               {ReactHtmlParser(props.children)}
+              Paragraphs are the building blocks of papers. Many students define
+              paragraphs in terms of length: a paragraph is a group of at least
+              five sentences, a paragraph is half a page long, etc. In reality,
+              though, the unity and coherence of ideas among sentences is what
+              constitutes a paragraph. A paragraph is defined as “a group of
+              sentences or a single sentence that forms a unit” (Lunsford and
+              Connors 116). Paragraphs are the building blocks of papers. Many
+              students define paragraphs in terms of length: a paragraph is a
+              group of at least five sentences, a paragraph is half a page long,
+              etc. In reality, though, the unity and coherence of ideas among
+              sentences is what constitutes a paragraph. In reality, though, the
+              unity and coherence of ideas among sentences is what constitutes a
+              paragraph. In reality, though, the unity and coherence of ideas
+              among sentences is what constitutes a paragraph.
             </div>
           </div>
-          <div class="w-100 pt-16 xs:pt-5 text-right xs:text-left">
-            <button class="btn bg-primary py-3 px-12 text-white rounded-full text-xs text-light mx-auto shadow-primary">
-              {props.buttonText}
-            </button>
-          </div>
+          <div class="w-100 pt-16 xs:pt-5 text-right xs:text-left"></div>
         </div>
-        <div class="w-50 xs:hidden pt-16 text-right">
+        <div class="w-50 xs:hidden text-right">
           <div class="text-right">
             <img src={"/img/group.png"} class="clip-full" />
           </div>
