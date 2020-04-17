f2300a10919e6247566f324654f003faed8eeb98
Yannick <musayann@octangrp.com>
2020-04-17 09:48:13 +0200

    Code cleanup

--- src/pages/service.js
+++ src/pages/service.js
@@ -3,13 +3,6 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Main from "../components/sections/main"
-import Service from "../components/sections/service"
-import Information from "../components/sections/information"
-import Team from "../components/sections/team"
-import Value from "../components/sections/value"
-import { graphql } from "gatsby"
-
 const ServicePage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
