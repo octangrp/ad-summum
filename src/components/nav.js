25617fe195af966dbf2761c7c3a4858d065cb3ee
Landry <landos@octangrp.com>
2020-05-20 22:22:07 +0200

    navbar-redesign

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,47 +2,53 @@ import { AnchorLink as Link } from "gatsby-plugin-anchor-links"
 import PropTypes from "prop-types"
 import React from "react"
 import { connect } from "react-redux"
-import Translator from "../utils/translator"
+import Logo from "../images/logo.png"
+
 class Navbar extends React.Component {
   render() {
-    const menu = Translator.process(
-      this.props.lang,
-      this.props.details.translations
-    )
     return (
-      <nav className="z-99 transition-500ms xs:hidden w-100 text-primary fixed bg-white m-0 t-0 l-0 r-0  border-0 border-b-1 border-solid border-primary ">
+      <nav className="z-99 transition-500ms w-100 text-black fixed bg-white m-0 t-0 l-0 r-0 ">
         <div className="xl:w-90 lg:w-90 xs:w-90 sm:w-90 md:w-90 flex mx-auto py-0 mx-0">
-          <ul className="list text-left w-85 lg:w-90  mr-auto p-0 m-0 hidden xl:block lg:block md:block px-0 mr-0  text-sm">
-            {menu.items.map((item, index) => (
+          <ul className="list text-right w-80 lg:w-90 font-primary font-semibold text-xs ml-auto p-0 m-0 hidden xl:block lg:block px-0 mr-0  text-sm">
+            {/* <li className="inline-block  text-center m-0 py-0 text-base pr-2">
+              <img src={Logo} alt="ad-summum-logo" width="50" />
+            </li> */}
+            {this.props.items.map((item, index) => (
               <li
                 key={index}
-                className="inline-block  text-center m-0 py-4 w-20 md:py-3 text-base hover:bg-primary hover:text-white"
+                className="inline-block  text-center m-0 py-4 text-sm "
               >
                 <Link
-                  to={item.url}
-                  className="inherit-color md:px-5 px-12 no-underline leading-none"
+                  to={item.link}
+                  className="inherit-color  px-6 no-underline leading-sm"
                 >
-                  {item.title}
+                  {item.label}
                 </Link>
               </li>
             ))}
           </ul>
-          <div className="w-10 text-right ml-auto pt-2 hidden xl:block lg:block md:block px-0 mr-0  text-sm text-right">
-            <img
-              src={"/img/united-states.svg"}
-              width="20"
-              onClick={() => this.props.switchLanguage("en_US")}
-              className="mt-3 md:mx-2 mx-4 cursor-pointer"
-              alt="english-language"
-            />
-            <img
-              src={"/img/france.svg"}
-              width="20"
-              onClick={() => this.props.switchLanguage("fr_FR")}
-              className="mt-3 cursor-pointer"
-              alt="french-language"
-            />
-          </div>
+          <ul className="list w-10 text-left font-semibold mr-auto hidden xl:block lg:block px-0 mr-0  text-sm ">
+            <li className="inline-block  m-0 py-4 px-3 text-xs">
+              <img
+                src={"/img/united-states.svg"}
+                width="20"
+                onClick={() => this.props.switchLanguage("en_US")}
+                className="cursor-pointer"
+                alt="english-language"
+              />
+              <span classNaeme="pl-2">EN</span>
+            </li>
+            <li className="inline-block m-0 py-4 px-3 text-xs ">
+              <img
+                src={"/img/france.svg"}
+                width="20"
+                onClick={() => this.props.switchLanguage("fr_FR")}
+                className="cursor-pointer"
+                alt="french-language"
+              />
+              FR
+            </li>
+          </ul>
         </div>
       </nav>
     )
@@ -51,17 +57,34 @@ class Navbar extends React.Component {
 
 Navbar.propTypes = {
   siteTitle: PropTypes.string,
-  details: PropTypes.array,
+  items: PropTypes.array,
 }
 
 Navbar.defaultProps = {
   siteTitle: ``,
-  details: [],
+  items: [
+    {
+      label: "HOME",
+      link: "/",
+    },
+    {
+      label: "TEAM",
+      link: "/#team",
+    },
+    {
+      label: "SERVICES",
+      link: "/#services",
+    },
+    {
+      label: "ABOUT US",
+      link: "/#about-us",
+    },
+    {
+      label: "AS BULLETIN",
+      link: "/#bulletin",
+    },
+  ],
 }
-
-const mapStateToProps = state => ({
-  lang: state.lang,
-})
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -73,4 +96,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default connect(null, mapDispatchToProps)(Navbar)
