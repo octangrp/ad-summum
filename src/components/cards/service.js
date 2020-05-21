e3ac6aea6ccaceeb764a6f09390d550a4e694a33
Yannick <musayann@octangrp.com>
2020-05-22 00:31:14 +0200

    Redesign service card

--- src/components/cards/service.js
+++ src/components/cards/service.js
@@ -12,33 +12,27 @@ class ServiceCard extends React.Component {
       this.props.details.translations
     )
     return (
-      <div className="w-30 xs:w-100 mx-auto">
-        <div className="w-100 xl:flex lg:flex xs:flex md:flex pt-12">
-          <div className="w-35 pr-4 flex items-center">
-            <div className="w-100">
-              <img src={details.image ? details.image.url : ""} />
-            </div>
+      <div className="w-30 xs:w-100 mb-10 mx-auto hover:shadow p-8 pb-18 relative hover:shadow-lg cursor-pointer transition-500ms border-1 border-solid boder-grey-lightest">
+        <div className="w-100 relative">
+          <img src={details.image ? details.image.url : ""} />
+        </div>
+        <div className="text-left text-black mx-auto relative pb-12">
+          <h4 className="text-xl text-grey-darker uppercase my-2 tracking-wider font-primary">
+            {details.title}
+          </h4>
+          <div className="w-100 text-lg my-5 tracking-wide ">
+            <TruncateHtml length={80} tokenize={"words"}>
+              {details.content}
+            </TruncateHtml>
           </div>
-
-          <div className="w-65">
-            <div className="text-left text-black mx-auto p-0">
-              <h4 className="text-primary text-xl font-primary font-normal font-light p-0 m-0 leading-loose sm:leading-normal xs:leading-normal">
-                {details.title}
-              </h4>
-              <div className="w-100 mx-auto xs:my-3 sm:text-sm text-base pt-2 m-0 xs:w-90 font-light ">
-                <TruncateHtml length={80} tokenize={"words"}>
-                  {details.content}
-                </TruncateHtml>
-              </div>
-            </div>
-            <div className="w-100 text-left pt-4">
-              <Link to={`/service/${details.slug}`}>
-                <button className="btn bg-none  py-2 px-4 text-primary rounded-full border-1 border-solid border-primary text-xs text-light mx-auto">
-                  {details.attributes.button_text}
-                </button>
-              </Link>
-            </div>
-          </div>
+        </div>
+        <div className="w-50 pt-4 text-right absolute b-10 r-0 px-8 pt-8">
+          <Link
+            to={`/service/${details.slug}`}
+            className="uppercase text-sm text-primary-darkest font-bold font-primary"
+          >
+            {details.attributes.button_text}
+          </Link>
         </div>
       </div>
     )
