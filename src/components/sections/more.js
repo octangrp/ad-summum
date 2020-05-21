70fb44538d1b2587e252f0df75c41440e5066d83
Landry <landos@octangrp.com>
2020-05-21 13:19:48 +0200

    changing the about us design

--- src/components/sections/more.js
+++ src/components/sections/more.js
@@ -1,27 +1,42 @@
 import PropTypes from "prop-types"
 import React from "react"
 import ReactHtmlParser from "react-html-parser"
-import SectionWrapper from "./sectionWrapper"
+import { Link } from "gatsby"
 
-const More = props => (
-  <SectionWrapper
-    id={props.id}
-    title={props.title}
-    showSubtitle={false}
-    backgroundClass="bg-primary text-white p-0 m-0"
-  >
-    {ReactHtmlParser(props.children)}
-  </SectionWrapper>
+const Service = props => (
+  <div className="w-100 relative bg-white relative">
+    <div className="w-100 ml-auto h-px-500 relative overflow-hidden">
+      <div className="flex w-85 mx-auto py-12">
+        <div className="w-40">
+          <div className="text-left">
+            <img src={"/img/logo-full.png"} className="w-60" />
+          </div>
+        </div>
+        <div className="w-60 my-auto border-0 border-b-1 border-r-1  border-solid border-primary">
+          <div className="text-left my-auto py-4 px-4">
+            <h1 className="text-xl font-primary font-semibold">ABOUT US</h1>
+            <div className=" w-20 border-1 border-grey-light border-solid"></div>
+            <p className="text-sm w-70">
+              ASC Ltd brings together with professional skills and extensive
+              experience in the areas of central banking, commercial banking and
+              financial system in general, legal issues and those related to
+              information technology and organization.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
-More.propTypes = {
+Service.propTypes = {
   siteTitle: PropTypes.string,
   buttonText: PropTypes.string,
 }
 
-More.defaultProps = {
+Service.defaultProps = {
   siteTitle: ``,
   buttonText: `Read more`,
 }
 
-export default More
+export default Service
