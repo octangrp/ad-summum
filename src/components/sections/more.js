210cbd3244846190317fb3f3464ac52311552225
Landry <landos@octangrp.com>
2020-05-26 18:39:24 +0200

    add image on about us

--- src/components/sections/more.js
+++ src/components/sections/more.js
@@ -7,9 +7,9 @@ import SectionTitle from "../section-title"
 import TruncateHtml from "../utilities/TruncateHtml"
 
 const Service = props => (
-  <div id={props.id} className="w-100 relative bg-white relative">
-    <div className="w-100 ml-auto relative overflow-hidden">
-      {/* <div className="flex w-85 mx-auto py-32">
+  // <div id={props.id} className="w-100 relative bg-white relative">
+  //   <div className="w-100 ml-auto relative overflow-hidden">
+  /* <div className="flex w-85 mx-auto py-32">
         <div className="w-40">
           <Logo />
         </div>
@@ -30,26 +30,25 @@ const Service = props => (
             </div>
           </div>
         </div>
-      </div> */}
+      </div> */
 
-      <div class="w-85 flex mx-auto h-px-600">
-        <div class="w-50">
-          <div class="h-px-600">
-            <img src={"/img/tim.png"} class="w-100 clip-full relative" />
-          </div>
-        </div>
-        <div class="w-50 border-0 border-b-1 border-t-1 border-r-1 border-solid border-black">
-          <div className="w-100 my-auto ">
-            <div className="text-left text-black my-auto p-12">
+  <div id={props.id} className="w-100 relative bg-white relative">
+    <div className="w-100 ml-auto  relative overflow-hidden">
+      <div className="w-100 h-100 bg-white absolute t-10 z-5 rotate-60deg -l-30" />
+      <div className="w-50 relative z-10">
+        <div className="w-90 ml-auto py-6">
+          <Logo />
+          <div className="w-100 my-auto py-6 relative">
+            <div className="text-left w-80 my-auto">
               <SectionTitle>{props.title}</SectionTitle>
-              <div className="text-lg font-secondary w-100 tracking-wider text-black leading-relaxed">
+              <div className="text-lg font-secondary w-100 tracking-wider leading-relaxed">
                 <TruncateHtml length={350} tokenize={"words"}>
                   {props.children}
                 </TruncateHtml>
               </div>
-              <div className="w-30 pt-12 text-right ml-auto">
+              <div className="absolute -r-10 b-5 text-right">
                 <Link to="about">
-                  <button class="btn btn-outline-primary hover:text-black focus:text-white py-2 px-4 text-primary text-sm text-light mx-auto">
+                  <button class="btn bg-white border-1 border-primary border-solid text-primary text-black  py-2 px-4 text-sm text-light mx-auto">
                     Read More
                   </button>
                 </Link>
@@ -57,6 +56,9 @@ const Service = props => (
             </div>
           </div>
         </div>
+      </div>
+      <div className="w-70 absolute r-0 t-0 h-100">
+        <img src={"/img/tim.png"} className="clip-full relative" />
       </div>
     </div>
   </div>
