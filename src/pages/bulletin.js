fa4267a74323006ed0f80a0c454cf81f613809d7
Landry <landos@octangrp.com>
2020-05-27 08:43:34 +0200

    blog responsiveness

--- src/pages/bulletin.js
+++ src/pages/bulletin.js
@@ -12,23 +12,23 @@ class BulletinPage extends React.Component {
         <SEO title="ASC Bulletin" />
         <div className="w-100 relative bg-white py-16">
           <div className="w-85 mx-auto  relative">
-            <div className="flex ">
+            <div className="xl:flex lg:flex md:flex">
               <div className="w-40">
                 <img src={"/img/logo-full.png"} className="w-80 relative" />
               </div>
               <div className="w-60 my-auto">
                 <div className="text-left my-auto">
-                  <h1 class="text-6xl">As Bulletin</h1>
+                  <h1 class="text-6xl xs:text-3xl">ASC Bulletin</h1>
                 </div>
               </div>
             </div>
 
-            <div className="pt-16">
+            <div className="pt-16 xs:pt-8">
               <div className="mx-auto pt-6 relative">
                 <img src={"/img/tim2.png"} class="w-100 relative" />
-                <div className="absolute flex w-100 b-0 l-0 image-gradient px-12 py-8">
-                  <div className="w-20 ml-auto text-white text-right mb-0 ">
-                    <h3 class="text-base ">jan-2-2020</h3>
+                <div className="absolute flex w-100 b-0 l-0 image-gradient px-12 xs:p-4 py-8">
+                  <div className="w-20 xs:w-50 ml-auto text-white text-right mb-0 ">
+                    <h3 class="text-base xs:text-xs">jan-2-2020</h3>
                   </div>
                 </div>
               </div>
@@ -40,20 +40,20 @@ class BulletinPage extends React.Component {
               <div class=" w-100 h-rem-1 rounded-full bg-grey-light"></div>
             </div>
 
-            <div class="flex ">
-              <div class="w-40">
-                <div class=" py-12 h-rem-86">
+            <div class="xl:flex lg:flex md:flex">
+              <div class="w-40 xs:w-100">
+                <div class=" py-12 xs:py-8 h-rem-86">
                   <img src={"/img/tim.png"} class="w-100 clip-full relative" />
                 </div>
               </div>
-              <div class="w-60 my-auto pl-12">
+              <div class="w-60 my-auto pl-12 xs:pl-0 xs:w-100">
                 <div class="text-left my-auto">
                   <div class=" flex w-100 font-primary">
                     <div class="w-10 my-auto">
                       <div class="w-rem-8 h-rem-8 bg-primary my-auto rounded-full"></div>
                     </div>
-                    <div class="w-90">
-                      <h1 class="text-2xl  font-bold uppercase">
+                    <div class="w-90 xs:pl-4">
+                      <h1 class="text-2xl xs:text-lg font-bold uppercase">
                         {" "}
                         Economics, Finance
                       </h1>
@@ -72,8 +72,8 @@ class BulletinPage extends React.Component {
               </div>
             </div>
 
-            <div class="w-85 flex mx-auto">
-              <div class="w-50 pl-12">
+            <div class="w-85 lg:w-100 md:w-100 xs:w-100 xl:flex lg:flex md:flex mx-auto">
+              <div class="w-50 pl-12 md:pl-0 md:pr-4 lg:pl-0 xs:pl-0 xs:w-100">
                 <div class=" pt-12 pb-4 h-rem-86">
                   <img src={"/img/tim.png"} class="w-100 clip-full relative" />
                 </div>
@@ -83,18 +83,18 @@ class BulletinPage extends React.Component {
                       <div class="w-rem-5 h-rem-5 bg-primary my-auto rounded-full"></div>
                     </div>
                     <div class="w-90">
-                      <h1 class="text-2xl  font-bold uppercase">
+                      <h1 class="text-2xl md:text-base xs:text-lg font-bold uppercase">
                         {" "}
                         Economics, Finance
                       </h1>
                     </div>
                   </div>
-                  <div class="text-base p-0 m-0 font-secondary w-100 tracking-wider leading-relaxed">
+                  <div class="text-base xs:text-xs p-0 m-0 font-secondary w-100 tracking-wider leading-relaxed">
                     <p class="p-0 m-0 text-left">Jan-12-2020</p>
                   </div>
                 </div>
               </div>
-              <div class="w-50 pl-12">
+              <div class="w-50 pl-12 xs:pl-0 xs:w-100">
                 <div class=" pt-12 pb-4 h-rem-86">
                   <img src={"/img/tim.png"} class="w-100 clip-full relative" />
                 </div>
@@ -104,13 +104,13 @@ class BulletinPage extends React.Component {
                       <div class="w-rem-5 h-rem-5 bg-primary my-auto rounded-full"></div>
                     </div>
                     <div class="w-90">
-                      <h1 class="text-2xl  font-bold uppercase">
+                      <h1 class="text-2xl md:text-base xs:text-lg font-bold uppercase">
                         {" "}
                         Economics, Finance
                       </h1>
                     </div>
                   </div>
-                  <div class="text-base p-0 m-0 font-secondary w-100 tracking-wider leading-relaxed">
+                  <div class="text-base xs:text-xs p-0 m-0 font-secondary w-100 tracking-wider leading-relaxed">
                     <p class="p-0 m-0 text-left">Jan-12-2020</p>
                   </div>
                 </div>
